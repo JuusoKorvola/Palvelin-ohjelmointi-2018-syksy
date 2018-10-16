8981d3df99969048153029e995cd59d245f4a3fc
juuso <juuso.korvola@gmail.com>
2018-10-16 23:14:38 +0300

    Modified some files as well, mainly populatedb and app

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Asennetaan mongoose liittymä
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://Juuso:Pottupottu1@ds034198.mlab.com:34198/express_local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-/* Create a new route in /routes/users.js that will display the text "You're so cool" at URL /users/cool/. Test it by running the server and visiting http://localhost:3000/users/cool/ in your browser. */
-router.get('/cool', function(req, res, next) {
-  res.send('Cool!');
+/* Challenge on PART 2: Create a new route in /routes/users.js that will display the text "You're so cool" at URL /users/cool/*/
+router.get('/cool/', function(req, res, next) {
+  res.send("You're so cool!");
 });
-
 
 module.exports = router;
